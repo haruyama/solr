bcaf416b12f93e318a75c89131d9d9ed61f1a7b1
HARUYAMA Seigo <haruyama@unixuser.org>
2011-04-30 16:49:03 +0900

    changed memory size.

--- evaluation_for_tokenizers/tomcat/bin/setenv.sh
+++ evaluation_for_tokenizers/tomcat/bin/setenv.sh
@@ -1,4 +1,4 @@
-export JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true -server -Xmx4G -Xms4G -XX:MaxPermSize=128m -XX:+UseGCOverheadLimit -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing -XX:CMSIncrementalDutyCycleMin=0 -XX:+UseParNewGC -XX:+UseStringCache -XX:+OptimizeStringConcat -XX:+UseCompressedStrings -XX:+UseTLAB -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:+CMSParallelRemarkEnabled"
+export JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true -server -Xmx2G -Xms2G -XX:MaxPermSize=128m -XX:+UseGCOverheadLimit -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing -XX:CMSIncrementalDutyCycleMin=0 -XX:+UseParNewGC -XX:+UseStringCache -XX:+OptimizeStringConcat -XX:+UseCompressedStrings -XX:+UseTLAB -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:+CMSParallelRemarkEnabled"
 export CATALINA_OUT=/dev/null
 
 # if Java heap size is over 32GB heap, remove -XX:+UseCompressedOops
