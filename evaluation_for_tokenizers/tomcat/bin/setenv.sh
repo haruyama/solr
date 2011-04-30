2420b9099adea46b3d1da73453ffd44a22f7cacc
HARUYAMA Seigo <haruyama@unixuser.org>
2011-04-30 15:39:51 +0900

    added setenv.sh like Fess.

---
+++ evaluation_for_tokenizers/tomcat/bin/setenv.sh
@@ -0,0 +1,5 @@
+export JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true -server -Xmx4G -Xms4G -XX:MaxPermSize=128m -XX:+UseGCOverheadLimit -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing -XX:CMSIncrementalDutyCycleMin=0 -XX:+UseParNewGC -XX:+UseStringCache -XX:+OptimizeStringConcat -XX:+UseCompressedStrings -XX:+UseTLAB -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:+CMSParallelRemarkEnabled"
+export CATALINA_OUT=/dev/null
+
+# if Java heap size is over 32GB heap, remove -XX:+UseCompressedOops
+# if your machine has 2 or more cpu, enabled -XX:+CMSParallelRemarkEnabled by -XX:+UseConcMarkSweepGC
